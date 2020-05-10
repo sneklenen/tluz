adfc2a6cf865562a006ee1c45227fb2a9cb04117
sneklenen <64986383+sneklenen@users.noreply.github.com>
2020-05-10 21:08:08 +0300

    Add files via upload

---
+++ 224/js/translater.js
@@ -0,0 +1,181 @@
+var new_lang = {
+	
+  
+  "lt0": "rewolucyjny <img alt=\"\" src=\"img/@2x/p1.png\"><span> za rachunek ketozy!</span>",
+  
+  "lt1": "tylko dziś!<span>rabat 50%</span>",
+  
+  "lt2": "nowa cena:<span class=\"price_main\"></span>",
+  
+  "lt3": "stara cena:<span class=\"price_old\"></span>",
+  
+  "lt4": "Do czasu zakończenia promocji:",
+  
+  "lt5": "Wybierz kraj:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+  
+  "lt6": "Wprowadź swoje imię:<input name=\"name\" placeholder=\"Agnieszka Nowak\" required=\"\" type=\"text\">",
+  
+  "lt7": " Wprowadź numer telefonu:<input name=\"phone\" placeholder=\"+48 552-27-65\" required=\"\" type=\"text\">",
+  
+  "lt8": "zamówić teraz",
+  
+  "lt9": "zamówić teraz",
+  
+  "lt10": "Bezpieczna utrata masy ciała <span> za rachunek ketozy </span>",
+  
+  "lt11": "<b> Ketoza - pozyskiwanie energii z tłuszczów, a nie z glukozy, która dostaje się do organizmu wraz z węglowodanami.</b> Jeśli ciało przestaje otrzymywać energię z węglowodanów, to po 7-14 dniach przechodzi w stan ketozy. W rezultacie rozpoczyna się aktywna transformacja tłuszczów w energię.",
+  
+  "lt12": "Jednak dieta polegająca na wykluczeniu węglowodanów z diety jest trudna do zaakceptowania przez ludzi.<br><span style=\"color: #9f116b; font-weight: 700;\">Z KETO DIET możesz rozpocząć proces udchudzania się bez torturowania siebie.</span> Kompleks zapewni dostawę ketonu <br> do ciała z zewnątrz, pomagając pozbyć się nadmiaru tłuszczu.",
+  
+  "lt13": "TWOJA",
+  
+  "lt14": "KETO",
+  
+  "lt15": "DIETA",
+  
+  "lt16": "keto<span>diet</span>",
+  
+  "lt17": "Obowiązkowy składnik diety.",
+  
+  "lt18": "Kapsułki powodują ketozę w organizmie i przekształcają zmagazynowane tłuszcze w energię.<b> Będziesz miał gwarancję, że będziesz schudnąć codziennie.</b>",
+  
+  "lt19": "WYGRYWANA KOMBINACJA <span class=\"big\">KETOZA+</span><span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt20": "Zazwyczaj ketoza zajmuje od 7 do 14 dni. Już 1 kapsułka KETO DIET skróci ten czas do 40-50 minut, podczas których rozpocznie się proces konwersji tłuszczów w energię i redukcji tkanki tłuszczowej. Po prostu bierz<span>KETO DIET codziennie i schudnij do 10-15 kg na miesiąc!</span>",
+  
+  "lt21": "etap 1",
+  
+  "lt22": "Ketoza pozwoli Ci spalić trudnodostępny tłuszcz przy jednoczesnym zachowaniu masy mięśniowej. Zmniejszenie spożycia węglowodanów, zapewnia stabilizację wagi i brak powrotu utraconych kilogramów.",
+  
+  "lt23": "etap 2",
+  
+  "lt24": "Keto Diet wyzwala działanie ketozy: przyspiesza metabolizm, zmniejsza apetyt i bezpośrednio atakuje zapasy tłuszczu!",
+  
+  "lt25": "zamówić teraz",
+  
+  "lt26": "<span class=\"big\">POTĘŻNE SKŁADNIKI </span>W SKŁADZIE<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt27": "Ketony Malinowe ",
+  
+  "lt28": "Działanie jest podobne do ciał ketonowych wytwarzanych przez ciało ludzkie. Jest niezwykle aktywne.<ul><li><span> Przekształca</span>tłuszcz w enegrię </li><li><span> Przyspiesza</span>metabolizm</li><li><span>Eluminuje</span>poczucie głodu</li></ul>",
+  
+  "lt29": "Beta sitosterol",
+  
+  "lt30": "Neutralizuje skutki niedożywienia <ul><li><span>Normalizuje</span> metabolizm lipidów </li><li><span>Oczszycza naczynia</span>od «złego» cholesterolu</li><li><span>Polepsza sylwetkę,</span> zwalcza cellulit </li></ul>",
+  
+  "lt31": "Koenzym Q10",
+  
+  "lt32": "Niedobór koenzymu Q10 jest przyczyną otyłości w 78% przypadków <ul><li><span>Normalizuje</span>metabolizm</li><li><span>Zmnijesza</span>ilość złego cholesterolu </li><li><span> Zwalnia </span> przejadanie się </li></ul>",
+  
+  "lt33": "L- karnityna ",
+  
+  "lt34": "Substancja związana z witaminami B jest syntetyzowana w ludzkim ciele <ul><li><span> Konwertuje </span> tłuszcze na energię </li> <li> <span> Łagodzi drażliwość, </span> poprawia humor </li> <li> <span> Łagodzi </span> depresję </li></ul>",
+  
+  "lt35": "Bromelaina ",
+  
+  "lt36": " Fermet pomagający w trawieniu białka i enzymów, wydzielany jest z miazgi i łodygi ananasa <ul><li><span> Aktywnie rozpuszcza </span> tłuszcz trzewny </li><li><span> Eliminuje </span> obrzęki </li> <li> <span> Zapobiega </span> otyłości narządów wewnętrznych </li></ul>",
+  
+  "lt37": "Organiczna synefryna ",
+  
+  "lt38": "Wyraźny środek lipolityczny. Blokuje aktywność receptorów alfa w komórkach tłuszczowych <ul><li><span> Walczy z «trwałym» tłuszczem, w tym </span></li><li><span> zwiększa zużycie </span> kalorii przez ciało </li><li><span> Poprawia wydajność </span> i wytrzymałość </li></ul>",
+  
+  "lt39": "OPINIA EKSPERTÓW O<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt40": "<span>Doroti Gudoll</span>, Amerykański dietetyk, praktyka zawodowa - 30 lat.",
+  
+  "lt41": "W procesie ketozy tłuszcze są rozkładane na kwasy tłuszczowe i glicerynę, które następnie przekształcane są w ciała ketonowe. Jednak ketoza jest wywoływana tylko w przypadku całkowitego wyczerpania zapasów glikogenu w wątrobie i tkance mięśniowej,<b> zwykle dzieje się to w ciągu 7-14 dni.</b>",
+  
+  "lt42": "Aby przyspieszyć ten proces, za którym idzie spalanie tłuszczu, polecam moim pacjentom suplement diety KETO DIET. KETO DIET doskonale wspiera ciało podczas diety ketonowej: wzbogaca ciało o niezbędne elementy, tłumi głód, eliminuje zmęczenie i niepokój.<b>KETO DIET zapewnia zdrową i naturalną utratę wagi, i nie mniej ważne, bez zwiotczenia skóry, co przyniesie wyjątkowe korzyści dla Twojego ciała.</b>",
+  
+  "lt43": "Główne właściwości KETO DIET to przyspieszenie przejścia ciała w stan ketozy i pomoc w przemianie tłuszczów w energię.",
+  
+  "lt44": "zamówić teraz",
+  
+  "lt45": "Dołącz się<b> do tysięcy ludzi,</b> którzy już wypróbowali <span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt46": "Opinie",
+  
+  "lt47": "Previous",
+  
+  "lt48": "Jak mało wiemy o naszych ciałach. Nie zastanawiałem się nawet, jakie zachodzą w nim złożone procesy, dopóki nie dotarłem do dietetyka i nie usłyszałem o ketozie. Jestem leniwą osobą. Nie mogę siedzieć na dietach, nie uprawiam też sport. Dlatego szukałem czegoś łatwego i szybkiego. O KETO DIET mogę powiedzieć jedno - musisz to spróbować! Dzięki temu środku udało mi się zrzucić rekordowe 15 kg i osiągnąć idealną sylwetkę.",
+  
+  "lt49": "Michał R, 34 lata",
+  
+  "lt50": "«O ile siebie pamiętam - zawsze jestem na diecie. Głód, sen i problemy ze skórą były moimi stałymi towarzyszami. Na wadze pojawił się minus, ale potem nastąpił ogromny plus i wszystko w kólko. Dowiedziałam się o KETO DIET od dietetyka. Wynikiem utraty wagi cieszyłam się niemal natychmiast. Co tydzień widziałam na wadze minus 3 kg. Dla mnie to była prawdziwa magia. Mój stan zdrowia był świetny, nie było głodu, spałam przez 7 godzin dziennie».",
+  
+  "lt51": "Dorota N, 27 lat",
+  
+  "lt52": "«Dla mnie KETO DIET było zbawieniem. Nigdy nie myślałam, że mogę jeść wszystko, co kocham, a jednocześnie schudnąć. Faktem jest, że zawsze miałam nadmiar węglowodanów w diecie. Co przekształciło się dla mnie w nadwagę 100 kg w ciągu 29 lat. Ogólnie rzecz biorąc, jak tylko nieznacznie dostosowałam ilość węglowodanów, waga zaczęła spadać. Ale nie mogę żyć bez węglowodanów, bo naprawdę lubię fast foody. Potem dowiedziałam się o KETO DIET. Po użyciu tego lekarstwa proces spalania nadmiaru wagi przebiegał znacznie szybciej i nawet nie musiałam rezygnować z węglowodanów. W sumie w ciągu 4 miesięcy schudłam 25 kg. DIETA KETO - po prostu strał w piąteczkę! Polecam wszystkim!»",
+  
+  "lt53": "Joanna K, 49 lat",
+  
+  "lt54": "Jak mało wiemy o naszych ciałach. Nie zastanawiałem się nawet, jakie zachodzą w nim złożone procesy, dopóki nie dotarłem do dietetyka i nie usłyszałem o ketozie. Jestem leniwą osobą. Nie mogę siedzieć na dietach, nie uprawiam też sport. Dlatego szukałem czegoś łatwego i szybkiego. O KETO DIET mogę powiedzieć jedno - musisz to spróbować! Dzięki temu środku udało mi się zrzucić rekordowe 15 kg i osiągnąć idealną sylwetkę.",
+  
+  "lt55": "Michał R, 34 lata",
+  
+  "lt56": "«O ile siebie pamiętam - zawsze jestem na diecie. Głód, sen i problemy ze skórą były moimi stałymi towarzyszami. Na wadze pojawił się minus, ale potem nastąpił ogromny plus i wszystko w kólko. Dowiedziałam się o KETO DIET od dietetyka. Wynikiem utraty wagi cieszyłam się niemal natychmiast. Co tydzień widziałam na wadze minus 3 kg. Dla mnie to była prawdziwa magia. Mój stan zdrowia był świetny, nie było głodu, spałam przez 7 godzin dziennie».",
+  
+  "lt57": "Dorota N, 27 lat",
+  
+  "lt58": "«Dla mnie KETO DIET było zbawieniem. Nigdy nie myślałam, że mogę jeść wszystko, co kocham, a jednocześnie schudnąć. Faktem jest, że zawsze miałam nadmiar węglowodanów w diecie. Co przekształciło się dla mnie w nadwagę 100 kg w ciągu 29 lat. Ogólnie rzecz biorąc, jak tylko nieznacznie dostosowałam ilość węglowodanów, waga zaczęła spadać. Ale nie mogę żyć bez węglowodanów, bo naprawdę lubię fast foody. Potem dowiedziałam się o KETO DIET. Po użyciu tego lekarstwa proces spalania nadmiaru wagi przebiegał znacznie szybciej i nawet nie musiałam rezygnować z węglowodanów. W sumie w ciągu 4 miesięcy schudłam 25 kg. DIETA KETO - po prostu strał w piąteczkę! Polecam wszystkim!»",
+  
+  "lt59": "Joanna K, 49 lat",
+  
+  "lt60": "Jak mało wiemy o naszych ciałach. Nie zastanawiałem się nawet, jakie zachodzą w nim złożone procesy, dopóki nie dotarłem do dietetyka i nie usłyszałem o ketozie. Jestem leniwą osobą. Nie mogę siedzieć na dietach, nie uprawiam też sport. Dlatego szukałem czegoś łatwego i szybkiego. O KETO DIET mogę powiedzieć jedno - musisz to spróbować! Dzięki temu środku udało mi się zrzucić rekordowe 15 kg i osiągnąć idealną sylwetkę.",
+  
+  "lt61": "Michał R, 34 lata",
+  
+  "lt62": "Next",
+  
+  "lt63": "Nadszedł czas <b>pozbyć się  skrępowań</b>",
+  
+  "lt64": "rewolucyjny <img alt=\"\" src=\"img/@2x/p1.png\"><span> za rachunek ketozy!</span>",
+  
+  "lt65": "tylko dziś!<span>rabat 50%</span>",
+  
+  "lt66": "nowa cena:<span class=\"price_main\"></span>",
+  
+  "lt67": "stara cena:<span class=\"price_old\"></span>",
+  
+  "lt68": "Do czasu zakończenia promocji:",
+  
+  "lt69": "Wybierz kraj:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+  
+  "lt70": "Wprowadź swoje imię:<input name=\"name\" placeholder=\"Иванова Ольга\" required=\"\" type=\"text\">",
+  
+  "lt71": " Wprowadź numer telefonu:<input name=\"phone\" placeholder=\"+7 (937) 552-27-65\" required=\"\" type=\"text\">",
+  
+  "lt72": "zamówić teraz ",
+  
+  "lt73": "Jak my pracujemy",
+  
+  "lt74": "ZŁOŻENIE ZAMÓWIENIA",
+  
+  "lt75": "Niezwłocznie po złożeniu zamówienia nasz menedżer skontaktuje się z Tobą w sprawie <br><b> potwierdzenia zamówienia </b>",
+  
+  "lt76": "DOSTAWA",
+  
+  "lt77": "Niezwłocznie po złożeniu zamówienia nasz menedżer skontaktuje się z Tobą w sprawie <br><b> potwierdzenia zamówienia</b>",
+  
+  "lt78": "OPŁATA",
+  
+  "lt79": "<b> Bez przedpłat!</b> Płatność za zamówieniу odbywa się ściśle po otrzymaniu towaru.",
+  
+  "lt80": "Gwarancja",
+  
+  "lt81": "Dokonując zakupu w naszym <br> sklepie internetowym, możesz być pewien wysokiej jakości towaru",
+  
+  
+  
+	
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
